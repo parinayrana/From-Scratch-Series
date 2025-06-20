c36a93a38f8293641bdbc767d7fe14ad675b02b3
Parinay Rana <parinayrana@gmail.com>
2025-06-20 22:25:28 +0530

    NaiveB Initiate

---
+++ ML_Models/Naive_Bayes.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class NaiveBayes:
+
+    def fit(self, X,y):
+        n_sample, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        self._mean = np.zeros((n_classes,n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes,n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for c in self._classes:
+            X_c = X[c==y]
+
+    def predict(self,X):
+        pass
